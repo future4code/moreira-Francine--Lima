aac23b773c367653b3bb13233c20be97c32f6d51
francine1919 <francine1919@gmail.com>
2022-03-28 15:57:23 -0300

    intro-express

---
+++ modulo4/intro-express/src/posts.ts
@@ -0,0 +1,14 @@
+export const posts = [
+  {
+    id: 10,
+    title: "oi",
+    body: "olá",
+    userId: 1,
+  },
+  {
+    id: 20,
+    title: "oiee",
+    body: "olárrr",
+    userId: 2,
+  },
+];
