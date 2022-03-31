cf2ef9d3cefad67a432acf1420b6b0ae14d9d37b
francine1919 <francine1919@gmail.com>
2022-03-31 17:55:39 -0300

    update

--- modulo4/fluxo-de-dados-backend/src/index.ts
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-
 import { AddressInfo } from "net";
 import { data } from "./data";
 import { Prod } from "./types";
@@ -27,33 +26,78 @@ app.post("/addprod", (req, res) => {
     name: req.body.name,
     price: req.body.price,
   };
-  if (req.body.name && req.body.price) {
-    //const newProds=[...data,newProd] //o spread faz o mesmo do codigo abaixo
+
+  if (
+    typeof req.body.name === "string" &&
+    typeof req.body.price === "number" &&
+    req.body.price > 0 &&
+    req.body.price &&
+    req.body.name
+  ) {
+    //const newProds=[...data,newProd] //esse exemplo com o spread faz o mesmo do codigo abaixo
     data.push(newProd); //.push retorna um numero o lenght da array
     res.status(201).send(data); //não posso passar números como argumento do .send(4)= err
+  } else if (req.body.price <= 0) {
+    throw new Error("O preço digitado deve ser um valor positivo."); //error 400 bad request
+  } else if (typeof req.body.name !== "string") {
+    throw new Error(
+      "O nome digitado não é uma string. O nome deve ser uma string." //error 400
+    );
+  } else if (typeof req.body.price !== "number") {
+    throw new Error(
+      "O preço digitado não é um número. O preço deve ser um número." //error 400
+    );
   } else {
-    throw new Error("Informações incompletas no body");
+    throw new Error("Informações incompletas ou incorretas no body"); //error 500 internal server error
   }
 });
-//4 Retorna todos os produtos
+
+//4
 app.get("/all", (req, res) => {
   res.status(200).send(data);
 });
+
 //5 Edita o preço
 app.put("/editprice/:id", (req, res) => {
-  const inputId = req.params.id;
-  const newPrice = req.body.price;
+  const inputId = Number(req.params.id);
+  const newPrice = Number(req.body.price);
 
-  data
-    .filter((prod) => {
-      return prod.id === Number(inputId);
-    })
-    .map((prod) => {
-      prod.price = newPrice;
-      return prod;
-    });
-  res.status(201).send(data);
+  const isProd = data.find((prod) => {
+    return inputId === prod.id;
+  });
+
+  console.log(isProd);
+  if (
+    isProd !== undefined &&
+    typeof req.body.price === "number" &&
+    req.body.price &&
+    req.body.price > 0
+  ) {
+    data
+      .filter((prod) => {
+        return prod.id === Number(inputId);
+      })
+      .map((prod) => {
+        prod.price = newPrice;
+        return prod;
+      });
+    res.status(201).send(data);
+  } else if (req.body.price < 0) {
+    throw new Error("O preço digitado deve ser um valor positivo."); //error 400 bad request
+  } else if (typeof req.body.price !== "number") {
+    throw new Error(
+      "O preço digitado não é um número. O preço deve ser um número." //error 400
+    );
+  } else if (isProd === undefined) {
+    throw new Error(
+      "O id digitado não correspende a um produto. Por favor forneça um id válido." //error 400
+    );
+  } else {
+    throw new Error("Ops ocorreu um erro"); //error 500 internal server error
+  }
 });
+//----------------------------
+//outra forma de fazer
 //   if (findProd) {
 //     // const prodChosen = data.filter((prod) => {
 //     //   return prod.id === Number(inputId);
@@ -82,8 +126,10 @@ app.delete("/removeprod/:idProd", (req, res) => {
       return prod.id !== Number(idProd);
     });
     res.send(newListProd);
+  } else if (findProd === undefined) {
+    throw new Error("Produto não encontrado, por favor insira um id válido");
   } else {
-    throw new Error("Insira um id válido");
+    throw new Error("Ops ocorreu um erro");
   }
 
   // const inputId = req.params.idProd;
