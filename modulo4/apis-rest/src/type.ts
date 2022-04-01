1ecfba514418c9bc68a1775c8750a31846939034
francine1919 <francine1919@gmail.com>
2022-03-31 21:46:31 -0300

    Atividade apis-rest

---
+++ modulo4/apis-rest/src/type.ts
@@ -0,0 +1,12 @@
+export type Users = {
+  id: number;
+  name: string;
+  email: string;
+  type: UserType;
+  age: number;
+};
+
+export enum UserType {
+  ADMIN = "ADMIN",
+  NORMAL = "NORMAL",
+}
